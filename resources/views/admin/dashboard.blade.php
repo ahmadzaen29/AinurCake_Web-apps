4b888a1d5f7e14ef50c7432f1f2d7dc9b6918422
didin <ahmadzaenalabidin29@gmail.com>
2024-05-24 05:53:33 +0700

    first commit

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -169,10 +169,57 @@
                 <!-- ============================================================== -->
                 <!-- Page Content -->
                 <!-- ============================================================== -->
-                <iframe title="Report Section" width="100%" height="600"
+                <!-- <iframe title="Report Section" width="100%" height="600"
                     src="https://app.powerbi.com/view?r=eyJrIjoiOWMxNDdkYmEtYjc0Mi00NTc4LTk2YWUtZjllMWY4MTk5MmE4IiwidCI6IjUyNjNjYzgxLTU5MTItNDJjNC1hYmMxLWQwZjFiNjY4YjUzMCIsImMiOjEwfQ%3D%3D"
-                    frameborder="0" allowFullScreen="true"></iframe>
+                    frameborder="0" allowFullScreen="true"></iframe> -->
+                <section class="content">
+                    <div class="row">
+                        <div class="col-md-3 col-sm6 col-xs-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outlet"></i></span>
 
+                                <div class="info-box-content">
+                                    <span class="info-box-text">CPU Traffic</span>
+                                    <span class="info-box-number">90<small>%</small></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Likes</span>
+                                    <span class="info-box-number">12121</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="clearfix visible-sm-block"></div>
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Sales</span>
+                                    <span class="info-box-number">234</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
+
+                                <div class="info-box-content">
+                                    <span class="info-box-text">New members</span>
+                                    <span class="info-box-number">2111</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
                 <!-- ============================================================== -->
                 <!-- footer -->
                 <!-- ============================================================== -->
